87efe4f81d86e98d086279b3bc9536abc2cbdb85
李有为 <willtrump@outlook.com>
2022-09-12 15:24:58 +0800

    second

--- AspectHelper/AspectHelper/AutoRetryAttribute.cs
+++ AspectHelper/AspectHelper/AutoRetryAttribute.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MethodBoundaryAspect.Fody.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace AspectHelper
 {
-    internal class AutoRetryAttribute
+    // 自动重试
+    public class AutoRetryAttribute : OnMethodBoundaryAspect
     {
     }
 }
